3f566011d1f318ba82eec88b6820174b742e7e0b
Xacalacaju <xavicamachoj@gmail.com>
2025-11-23 15:16:13 +0100

    Updated elo.js because older one didn't work idk why

--- elo.js
+++ elo.js
@@ -1,17 +1,7 @@
-export function calculateNewRatings(rA, rB, winner, K=32) {
-  const Ea = 1 / (1 + Math.pow(10, (rB - rA) / 400));
-  const Eb = 1 / (1 + Math.pow(10, (rA - rB) / 400));
-  let newA = rA, newB = rB;
-  if (winner === 'A') {
-    newA = Math.round(rA + K * (1 - Ea));
-    newB = Math.round(rB + K * (0 - Eb));
-  } else if (winner === 'B') {
-    newA = Math.round(rA + K * (0 - Ea));
-    newB = Math.round(rB + K * (1 - Eb));
-  } else {
-    // draw
-    newA = Math.round(rA + K * (0.5 - Ea));
-    newB = Math.round(rB + K * (0.5 - Eb));
-  }
-  return { newA, newB, changeA: newA - rA, changeB: newB - rB };
+// utils/elo.js
+export function updateELO(player1Rating, player2Rating, winner) {
+  // ejemplo sencillo: suma o resta 10 puntos
+  if (winner === 1) return [player1Rating + 10, player2Rating - 10]
+  if (winner === 2) return [player1Rating - 10, player2Rating + 10]
+  return [player1Rating, player2Rating]
 }
